9be72ab33c2f1b83c1182872c1cb8a3936015e51
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-08-24 04:47:37 +0900

    Use elsif

--- Formula/git-branch-delete-merged.rb
+++ Formula/git-branch-delete-merged.rb
@@ -10,8 +10,7 @@ class GitBranchDeleteMerged < Formula
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/nekonenene/git-branch-delete-merged-rs/releases/download/v1.0.1/git-branch-delete-merged-Darwin-arm64"
       sha256 "e0af9bbd94a530377c932860289af2d570339fc26fcb3a2b86b38ccd7924660e"
-    end
-    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+    elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/nekonenene/git-branch-delete-merged-rs/releases/download/v1.0.1/git-branch-delete-merged-Darwin-x86_64"
       sha256 "4782dc42eac66a0a687a470479a27a7b21162deff5d8903d5bbbdcfae55d8e6a"
     end
@@ -21,8 +20,7 @@ class GitBranchDeleteMerged < Formula
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/nekonenene/git-branch-delete-merged-rs/releases/download/v1.0.1/git-branch-delete-merged-Linux-arm64"
       sha256 "54bedf6e6b1ac2d43a93706bada5ebd78e61b043f6c94fa274e5ef858d337d51"
-    end
-    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+    elsif Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/nekonenene/git-branch-delete-merged-rs/releases/download/v1.0.1/git-branch-delete-merged-Linux-x86_64"
       sha256 "325890012b989393a5974974692fbe59b93f3d615debb5400ce0731015dfc93a"
     end
